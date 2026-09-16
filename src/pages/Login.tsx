0928bac283bc33ea8a10caa25ec5848c18c4325a
Nguyen Duy Son <uchihason5@gmail.com>
2026-09-16 14:35:16 +0700

    Done Login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,83 @@
+import { Button, Card } from "@heroui/react";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { useAuth } from "../contexts/AuthContext";
+
+export default function Login() {
+  const navigate = useNavigate();
+  const { login } = useAuth();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsloading] = useState(false);
+  const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isLoading) return;
+    setIsloading(true);
+    try {
+      await login(username, password);
+      navigate("/products");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsloading(false);
+    }
+  };
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-linear-to-br from-blue-50 to-indigo-100">
+      <Card className="w-full max-w-sm shadow-xl">
+        <Card.Header className="flex flex-col items-start gap-1 px-6 pt-6 pb-0">
+          <h1 className="text-2xl font-bold text-gray-800">Welcome back</h1>
+          <p className="text-sm text-gray-500">Sign in to Product Manager</p>
+        </Card.Header>
+        <Card.Content className="px-6 pt-4 pb-6">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-1">
+              <label
+                htmlFor="username"
+                className="text-sm font-medium text-gray-700"
+              >
+                Username
+              </label>
+              <input
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                id="username"
+                type="text"
+                placeholder="Enter your username"
+                autoComplete="username"
+                className="rounded-lg border border-gray-300 px-3 py-2 text-sm outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <label
+                htmlFor="password"
+                className="text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                id="password"
+                type="password"
+                placeholder="Enter your password"
+                autoComplete="current-password"
+                className="rounded-lg border border-gray-300 px-3 py-2 text-sm outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            <Button
+              isDisabled={isLoading}
+              type="submit"
+              variant="primary"
+              className="mt-2 w-full"
+            >
+              Sign In
+            </Button>
+          </form>
+        </Card.Content>
+      </Card>
+    </div>
+  );
+}
---
+++ src/routes.tsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter, Navigate } from "react-router";
+import Layout from "./components/Layout";
+import Login from "./pages/Login";
+import ProductsPage from "./pages/Products/ProductsPage";
+import ProductDetailPage from "./pages/Products/ProductDetailPage";
+import GuestRoute from "./components/GuestRouts";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/products" replace />,
+  },
+  {
+    Component: GuestRoute,
+    children: [
+      {
+        path: "/login",
+        Component: Login,
+      },
+    ],
+  },
+  {
+    Component: ProtectedRoute,
+    children: [
+      {
+        Component: Layout,
+        children: [
+          {
+            path: "/products",
+            Component: ProductsPage,
+          },
+          {
+            path: "/products/:id",
+            Component: ProductDetailPage,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+export default routes;
